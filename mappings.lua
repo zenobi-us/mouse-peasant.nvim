caad93fbd6c5512e21d4d2c11ace6f5b6f883f77
Zenobius Jiricek <airtonix@users.noreply.github.com>
2023-05-10 10:58:24 +0930

    fix: ensure function is local only

--- mappings.lua
+++ mappings.lua
@@ -5,7 +5,7 @@
 -- automatically pick-up stored data by this setting.)
 --
 --
-function close_buffers()
+local close_buffers = function()
   local bufs = vim.fn.getbufinfo { buflisted = true }
   require("astronvim.utils.buffer").close(0)
   if require("astronvim.utils").is_available "alpha-nvim" and not bufs[2] then require("alpha").start(true) end
