1581052f11a4d1b6176bda664909673c0693c5d4
Zeno Jiricek <airtonix@users.noreply.github.com>
2023-05-12 10:41:51 +0930

    fix: imports

--- core/popup/init.lua
+++ core/popup/init.lua
@@ -1,8 +1,8 @@
 local M = {}
 
-M.constants = require "core.popup.constants"
-M.predicate = require "core.popup.predicate"
-M.render = require "core.popup.render"
+M.constants = require "user.core.popup.constants"
+M.predicate = require "user.core.popup.predicate"
+M.render = require "user.core.popup.render"
 
 M.render.clear_menu "PopUp"
 
